ac07144150480c6f7555fd2d189c91e25747ce36
Homer Lu <hlu4@logitech.com>
2025-04-24 12:36:37 +0800

    202504241236

--- lvxu/lvxu.cpp
+++ lvxu/lvxu.cpp
@@ -8,7 +8,6 @@
 #include <ksproxy.h>
 #include <vidcap.h>
 #include <ksmedia.h>
-
 #include <cfgmgr32.h>
 
 #pragma comment(lib, "strmiids")
@@ -47,8 +46,8 @@ static const GUID DEV_SPECIFIC = { 0x941C7AC0, 0xC559, 0x11D0, { 0x8A, 0x2B, 0x0
 static void Help()
 {
     printf("Syntax:\n");
-    printf("  LVXU.EXE <xu-name> read <cs>\n");
-    printf("  - to read extension unit\n");
+    printf("  LVXU.EXE <xu-name> write <cs>\n");
+    printf("  - to write extension unit\n");
     printf("  LVXU.EXE <xu-name> write <cs> <data>\n");
     printf("  - to write extension unit\n");
     printf("Options:\n");
@@ -59,7 +58,7 @@ static void Help()
 
 static int hexstr2array(const char* ptr, UCHAR* Data)
 {
-    char tmp[3];
+    char tmp[3] = {0};
     size_t len = strlen(ptr);
     int cnt = 0;
 
@@ -86,52 +85,7 @@ static int hexstr2array(const char* ptr, UCHAR* Data)
     return cnt;
 }
 
-
-
-
-#if 0
-// Helper: Get DevicePath from SetupAPI
-std::wstring GetDevicePathFromFriendlyName(const std::wstring& targetName) {
-    HDEVINFO hDevInfo = SetupDiGetClassDevs(&KSCATEGORY_CAPTURE, nullptr, nullptr, DIGCF_PRESENT | DIGCF_DEVICEINTERFACE);
-    if (hDevInfo == INVALID_HANDLE_VALUE) return L"";
-
-    SP_DEVICE_INTERFACE_DATA ifData = { 0 };
-    ifData.cbSize = sizeof(ifData);
-
-    WCHAR nameBuf[512];
-    WCHAR deviceIDBuf[512];
-
-    for (DWORD i = 0; SetupDiEnumDeviceInterfaces(hDevInfo, nullptr, &KSCATEGORY_CAPTURE, i, &ifData); ++i) {
-        DWORD requiredSize = 0;
-        SetupDiGetDeviceInterfaceDetail(hDevInfo, &ifData, nullptr, 0, &requiredSize, nullptr);
-
-        std::vector<BYTE> detailBuffer(requiredSize);
-        auto detail = (PSP_DEVICE_INTERFACE_DETAIL_DATA)detailBuffer.data();
-        detail->cbSize = sizeof(SP_DEVICE_INTERFACE_DETAIL_DATA);
-
-        SP_DEVINFO_DATA devData = { 0 };
-        devData.cbSize = sizeof(devData);
-
-        if (SetupDiGetDeviceInterfaceDetail(hDevInfo, &ifData, detail, requiredSize, nullptr, &devData)) {
-            SetupDiGetDeviceRegistryProperty(hDevInfo, &devData, SPDRP_FRIENDLYNAME, nullptr,
-                (PBYTE)nameBuf, sizeof(nameBuf), nullptr);
-
-            if (targetName == nameBuf) {
-                // Get Device Instance ID
-                CM_Get_Device_IDW(devData.DevInst, deviceIDBuf, MAX_DEVICE_ID_LEN, 0);
-                SetupDiDestroyDeviceInfoList(hDevInfo);
-                std::wcout << L"  [DeviceInstanceID] " << deviceIDBuf << std::endl;
-                return detail->DevicePath;
-            }
-        }
-    }
-
-    SetupDiDestroyDeviceInfoList(hDevInfo);
-    return L"";
-}
-#endif
-
-DWORD GetNodeNumber(IBaseFilter *pFilter)
+static DWORD GetNodeNumber(IBaseFilter *pFilter)
 {
     DWORD nNodes = 0;
     HRESULT hr;
@@ -146,6 +100,110 @@ Return:
     return nNodes;
 }
 
+static int ParseArguments(int argc, const char** argv, int& VID, int& PID, int& Intf, const char*& xuName, GUID& guid, bool& write, ULONG& ControlSelector, UCHAR *Data, ULONG &DataLen)
+{
+    int inState = 0;
+
+    VID = -1;
+    PID = -1;
+    Intf = -1;
+    DataLen = 0;
+
+    while (argc) {
+        if (strcmp(argv[0], "--vid") == 0) {
+            argc--;
+            argv++;
+            if (argc == 0) {
+                printf("syntax error!\n");
+                return false;
+            }
+            VID = strtol(argv[0], NULL, 16);
+        }
+        else if (strcmp(argv[0], "--pid") == 0) {
+            argc--;
+            argv++;
+            if (argc == 0) {
+                printf("syntax error!\n");
+                return false;
+            }
+            PID = strtol(argv[0], NULL, 16);
+        }
+        else if (strcmp(argv[0], "--intf") == 0) {
+            argc--;
+            argv++;
+            if (argc == 0) {
+                printf("syntax error!\n");
+                return false;
+            }
+            Intf = strtol(argv[0], NULL, 0);
+        }
+        else if (argv[0][0] == '-') {
+            printf("unsupported option: \"%s\"\n", argv[0]);
+            return false;
+        }
+        else {
+            switch (inState) {
+            case 0: // GUID
+                if (strcmp(argv[0], "infoxu") == 0) {
+                    guid = LVXU_DEVICE_INFO_GUID;
+                    xuName = argv[0];
+                }
+                else if (strcmp(argv[0], "testxu") == 0) {
+                    guid = LVXU_TEST_GUID;
+                    xuName = argv[0];
+                }
+                else if (strcmp(argv[0], "videoxu") == 0) {
+                    guid = LVXU_VIDEO_GUID;
+                    xuName = argv[0];
+                }
+                else if (strcmp(argv[0], "pcxu") == 0) {
+                    guid = LVXU_PERIPHERAL_GUID;
+                    xuName = argv[0];
+                }
+                else {
+                    printf("syntax error: \"%s\" isn't an XU\n", argv[0]);
+                    return false;
+                }
+                break;
+            case 1: // read|write
+                if (strcmp(argv[0], "write") == 0) {
+                    write = true;
+                }
+                else if (strcmp(argv[0], "read") == 0) {
+                    write = false;
+                }
+                else {
+                    printf("syntax error: the 3rd parameter (%s) must be \"read\" or \"write\"\n", argv[0]);
+                    return false;
+                }
+                break;
+            case 2: // CS
+                ControlSelector = strtol(argv[0], NULL, 0);
+                if (ControlSelector == 0) {
+                    printf("syntax error: \"%s\" isn't a valid control selector number\n", argv[0]);
+                    return false;
+                }
+                break;
+            case 3:
+                if (write) {
+                    DataLen = hexstr2array(argv[0], Data);
+                }
+                break;
+            }
+            inState++;
+        }
+        argc--;
+        argv++;
+    }
+
+    if (write && inState != 4 || !write && inState != 3) {
+        printf("syntax error!\n");
+        return false;
+    }
+
+    return true;
+}
+
 
 int main(int argc, const char **argv)
 {
@@ -156,17 +214,19 @@ int main(int argc, const char **argv)
     IMoniker* pMoniker = NULL;
     IBindCtx* pBindCtx = NULL;
     DWORD nNodes;
-    KSP_NODE XU;
+    KSP_NODE XU = { 0 };
     ULONG xuLength = 0;
     ULONG byteReturned = 0;
     ULONG ControlSelector;
-    bool read = false;
+    bool write = false;
+    bool done = false;
     UCHAR Data[64] = { 0 };
     ULONG DataLen = 0;
     int VID = -1;
     int PID = -1;
     int Intf = -1;
     int inState = 0;
+    const char* xuName = NULL;
 
     // skip argv[0]
     argc--;
@@ -177,89 +237,8 @@ int main(int argc, const char **argv)
         return hr;
     }
 
-    while (argc) {
-        if (strcmp(argv[0], "--vid") == 0) {
-            argc--;
-            argv++;
-            if (argc == 0) break;
-            VID = strtol(argv[0], NULL, 16);
-        }
-        else if (strcmp(argv[0], "--pid") == 0) {
-            argc--;
-            argv++;
-            if (argc == 0) break;
-            PID = strtol(argv[0], NULL, 16);
-        }
-        else if (strcmp(argv[0], "--intf") == 0) {
-            argc--;
-            argv++;
-            if (argc == 0) break;
-            Intf = strtol(argv[0], NULL, 0);
-        }
-        else if(argv[0][0]=='-') {
-            printf("unsupported option: \"%s\"\n", argv[0]);
-            Help();
-            goto Return;
-        }
-        else {
-            switch (inState) {
-            case 0: // GUID
-                if (strcmp(argv[0], "infoxu") == 0) {
-                    XU.Property.Set = LVXU_DEVICE_INFO_GUID;
-                }
-                else if (strcmp(argv[0], "testxu") == 0) {
-                    XU.Property.Set = LVXU_TEST_GUID;
-                }
-                else if (strcmp(argv[0], "videoxu") == 0) {
-                    XU.Property.Set = LVXU_VIDEO_GUID;
-                }
-                else if (strcmp(argv[0], "pcxu") == 0) {
-                    XU.Property.Set = LVXU_PERIPHERAL_GUID;
-                }
-                else {
-                    printf("syntax error: \"%s\" isn't an XU\n", argv[0]);
-                    Help();
-                    goto Return;
-                }
-                break;
-            case 1: // read|write
-                if (strcmp(argv[0], "read") == 0) {
-                    read = true;
-                }
-                else if (strcmp(argv[0], "write") == 0) {
-                    read = false;
-                }
-                else {
-                    printf("syntax error: \"%s\" must be read or write\n", argv[0]);
-                    Help();
-                    goto Return;
-                }
-                break;
-            case 2: // CS
-                ControlSelector = strtol(argv[0], NULL, 0);
-                if (ControlSelector == 0) {
-                    printf("syntax error: \"%s\" isn't a valid control selector\n", argv[0]);
-                    Help();
-                    goto Return;
-                }
-                break;
-            }
-            inState++;
-            if (inState == 3) break;
-        }
-        argc--;
-        argv++;
-    }
-
-    if (!read) {
-        if (argc <= 0) {
-            printf("syntax error: write command, but data doesn't exist\n");
-            Help();
-            goto Return;
-        }
-        DataLen = hexstr2array(argv[0], Data);
-        argc--;
-        argv++;
+    if (!ParseArguments(argc, argv, VID, PID, Intf, xuName, XU.Property.Set, write, ControlSelector, Data, DataLen)) {
+        goto Return;
     }
 
     hr = CreateBindCtx(NULL, &pBindCtx);
@@ -282,20 +261,38 @@ int main(int argc, const char **argv)
 
     if (pEnum) {
         DWORD cFetched;
-        while ((hr = pEnum->Next(1, &pMoniker, &cFetched)) == S_OK)
+        while (!done && (hr = pEnum->Next(1, &pMoniker, &cFetched)) == S_OK)
         {
             LPOLESTR wszDisplayName = NULL;
-            IBaseFilter* pFilter;
+            IBaseFilter* pFilter = NULL;
             IKsControl* pKsControl = NULL;
 
-            pMoniker->GetDisplayName(pBindCtx, NULL, &wszDisplayName);
-            wcout << wszDisplayName << endl;
+            // parse DisplayName to get VID, PID, Interface
+            hr = pMoniker->GetDisplayName(pBindCtx, NULL, &wszDisplayName);
+            if (FAILED(hr)) goto Next;
+            // @device:pnp:\\?\usb#vid_046d&pid_094f&mi_00#6&2c308b75&4&0000#{65e8773d-8f56-11d0-a3b9-00a0c9223196}\global
+            WCHAR* wstr;
+            if (VID != -1) {
+                wstr = wcsstr(wszDisplayName, L"#vid_");
+                if (!wstr) goto Next;
+                if (wcstoul(wstr + 5, NULL, 16) != VID) goto Next;
+            }
+            if (PID != -1) {
+                wstr = wcsstr(wszDisplayName, L"&pid_");
+                if (!wstr) goto Next;
+                if (wcstoul(wstr + 5, NULL, 16) != PID) goto Next;
+            }
+            if (Intf != -1) {
+                wstr = wcsstr(wszDisplayName, L"&mi_");
+                if (!wstr) goto Next;
+                if (wcstoul(wstr + 4, NULL, 16) != Intf) goto Next;
+            }
 
             hr = pMoniker->BindToObject(pBindCtx, NULL, __uuidof(IBaseFilter), (void**)&pFilter);
-            if (FAILED(hr)) continue;
+            if (FAILED(hr)) goto Next;
 
             hr = pFilter->QueryInterface(__uuidof(IKsControl), (void**)(&pKsControl));
-            if (FAILED(hr)) goto Return;
+            if (FAILED(hr)) goto Next;
 
             // find NodeID
             nNodes = GetNodeNumber(pFilter);
@@ -308,47 +305,72 @@ int main(int argc, const char **argv)
                     break;
                 }
             }
-            if (XU.NodeId == nNodes) continue;
+            if (XU.NodeId == nNodes) goto Next;
 
             // get control length
             XU.Property.Id = ControlSelector;
             XU.Property.Flags = KSPROPERTY_TYPE_GET | KSPROPERTY_TYPE_TOPOLOGY;
             hr = pKsControl->KsProperty((KSPROPERTY*)&XU, sizeof(XU), NULL, 0, &xuLength);
-            if (hr != HRESULT_FROM_WIN32(ERROR_MORE_DATA)) goto Return;
+            if (hr != HRESULT_FROM_WIN32(ERROR_MORE_DATA)) goto Next;
 
             // access data
-            if (read) {
-                XU.Property.Flags = KSPROPERTY_TYPE_GET | KSPROPERTY_TYPE_TOPOLOGY;
-                hr = pKsControl->KsProperty((KSPROPERTY*)&XU, sizeof(XU), Data, sizeof(Data), &xuLength);
-                if (FAILED(hr)) goto Return;
-                for (UWORD i = 0; i < xuLength; i++) {
-                    printf("%02X", Data[i]);
-                }
-                printf("\n");
-                // break;
-            }
-            else {
+            if (write) {
                 XU.Property.Flags = KSPROPERTY_TYPE_SET | KSPROPERTY_TYPE_TOPOLOGY;
                 hr = pKsControl->KsProperty((KSPROPERTY*)&XU, sizeof(XU), Data, sizeof(Data), &xuLength);
-                if (FAILED(hr)) goto Return;
+                if (SUCCEEDED(hr)) {
+                    done = true;
+                }
+            }
+            else {
+                XU.Property.Flags = KSPROPERTY_TYPE_GET | KSPROPERTY_TYPE_TOPOLOGY;
+                hr = pKsControl->KsProperty((KSPROPERTY*)&XU, sizeof(XU), Data, sizeof(Data), &xuLength);
+                if (SUCCEEDED(hr)) {
+                    for (UWORD i = 0; i < xuLength; i++) {
+                        printf("%02X", Data[i]);
+                    }
+                    printf("\n");
+                    done = true;
+                }
             }
 
-
-            pKsControl->Release();
-            pKsControl = NULL;
-            pFilter->Release();
-            pFilter = NULL;
-            CoTaskMemFree(wszDisplayName);
-            wszDisplayName = NULL;
+        Next:
+            if (pKsControl) pKsControl->Release();
+            if (pFilter) pFilter->Release();
+            if (wszDisplayName) CoTaskMemFree(wszDisplayName);
             pMoniker->Release();
-            pMoniker = NULL;
         }
+    }
+    if (!done) {
+        if (write) {
+            printf("failed to write");
+            for (ULONG i = 0; i < DataLen; i++) {
+                printf(" %02X", Data[i]);
+            }
+            printf("to");
+        }
+        else {
+            printf("failed to read from");
+        }
+        if (VID != -1) {
+            printf(" vid=%04x", VID);
+        }
+        if (PID != -1) {
+            printf(" pid=%04x", VID);
+        }
+        if (Intf != -1) {
+            printf(" interface=%d", Intf);
+        }
+        printf(" <%s> cs=%d", xuName, ControlSelector);
+        printf("\n");
     }
 
 Return:
-    if (pBindCtx) pBindCtx->Release();
-    if (pMoniker) pMoniker->Release();
+    if (FAILED(hr)) {
+        printf("error=%d\n", hr);
+    }
     if (pEnum) pEnum->Release();
+    if (pDevEnum) pDevEnum->Release();
+    if (pBindCtx) pBindCtx->Release();
     CoUninitialize();
     return 0;
 }
